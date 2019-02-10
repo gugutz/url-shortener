6aa9b469f7d9c8f2057a92e661572c2811cf4db3
gugutz <gugutz@gmail.com>
2019-02-10 00:39:40 -0200

    chore: made repo exclusive for java version of the app, removed elixir and others

--- elixir/url/lib/repo/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule Repo do
-  use Ecto.Repo,
-    otp_app: :url,
-    adapter: Ecto.Adapters.Postgres
-end
--- elixir/url/test/url_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule URLTest do
-  use ExUnit.Case
-  doctest URL
-
-  test "greets the world" do
-    assert URL.hello() == :world
-  end
-end
